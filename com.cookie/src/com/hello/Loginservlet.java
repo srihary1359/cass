e4b450861caf815498dda819040b8cad2c26d9cd
kavitha1359 <kavitha1359@LAPTOP-GJO1VBHE>
2021-01-30 13:35:12 +0700

    recived

---
+++ com.cookie/src/com/hello/Loginservlet.java
@@ -0,0 +1,15 @@
+package com.hello;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Loginservlet extends HttpServlet{
+
+public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+}
+}
